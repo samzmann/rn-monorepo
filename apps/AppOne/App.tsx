c13107be6155fa51e8fd799d98ab933ac478fb9d
Samuel Mann <samuelcmann@gmail.com>
2023-02-08 11:54:14 +0100

    add correct metro configs, add rn-fast-image dep to AppOne

--- apps/AppOne/App.tsx
+++ apps/AppOne/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {SafeAreaView, ScrollView,} from 'react-native';
+import {ContainerFastImage} from "@vytal/vytal-container-tools";
+import {ContainerTypeId} from "@vytal/vytal-container-tools/types";
 
 const App = () => {
 
   return (
     <SafeAreaView style={{backgroundColor: 'white'}}>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-
+        <ContainerFastImage
+          width={100}
+          height={100}
+          containerTypeId={ContainerTypeId.Ctr1250mlBowl}
+        />
       </ScrollView>
     </SafeAreaView>
   );
--- apps/AppOne/metro.config.js
+++ apps/AppOne/metro.config.js
@@ -1,17 +1,45 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
  *
  * @format
  */
+const path = require('path')
+const { getDefaultConfig } = require('metro-config')
+const { getMetroAndroidAssetsResolutionFix, getMetroTools } = require('react-native-monorepo-tools')
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+const monorepoMetroTools = getMetroTools()
+
+const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix({
+  depth: 3,
+})
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig()
+  return {
+    transformer: {
+      // babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+      publicPath: androidAssetsResolutionFix.publicPath,
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+      blockList: monorepoMetroTools.blockList,
+      extraNodeModules: monorepoMetroTools.extraNodeModules,
+    },
+    projectRoot: path.resolve(__dirname),
+    watchFolders: monorepoMetroTools.watchFolders,
+    server: {
+      enhanceMiddleware: middleware => androidAssetsResolutionFix.applyMiddleware(middleware),
+    },
+  }
+})()
